f67977ba264daef0ec0fed56d217868acbbd8b8a
oni1997 <dzidzaimaenza@gmail.com>
2025-03-13 09:07:50 +0200

    Update resume details with new job information and personal name change

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._company = "Microsoft";
+        job1._company = "APS Holdings";
         job1._jobTitle = "Software Engineer";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
         Job job2 = new Job();
-        job2._company = "Apple";
-        job2._jobTitle = "Manager";
+        job2._company = "APS Holdings";
+        job2._jobTitle = "Software Engineer II";
         job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._endYear = 2025;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Onesmus Maenzanise";
         
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
