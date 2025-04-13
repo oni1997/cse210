809ebb6f25e84c64e806739a9e98e634502b03e6
oni1997 <dzidzaimaenza@gmail.com>
2025-04-13 17:02:21 +0200

    Implement Shape hierarchy with Square, Rectangle, Circle classes and area calculation

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+        Circle circle = new Circle("Green", 6);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
